ee78d7b3179b39d664dae3c183e3c8c4bcb8ce9e
羽航 <geyuhang.pt@alibaba-inc.com>
2015-10-27 10:43:05 +0800

    Updates

--- examples/vertical.js
+++ examples/vertical.js
@@ -3,22 +3,22 @@ webpackJsonp([4],{
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(169);
+	module.exports = __webpack_require__(168);
 
 
 /***/ },
 
-/***/ 169:
+/***/ 168:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-steps/assets/index.css\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	__webpack_require__(2);
-	__webpack_require__(3);
 	
-	var React = __webpack_require__(4);
-	var ReactDOM = __webpack_require__(160);
-	var Steps = __webpack_require__(161);
+	var React = __webpack_require__(3);
+	var ReactDOM = __webpack_require__(159);
+	var Steps = __webpack_require__(160);
 	
 	var container = document.getElementById('__react-content');
 	
