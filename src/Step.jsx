04bd632d6ef9a6725d71cc6521c4b56a75f44fa2
afc163 <afc163@gmail.com>
2017-02-28 14:53:10 +0800

    refactor progressDot tail width

--- src/Step.jsx
+++ src/Step.jsx
@@ -57,6 +57,7 @@ export default class Step extends React.Component {
           : <div
             ref="tail"
             className={`${prefixCls}-tail`}
+            style={{ paddingRight: -adjustMarginRight }}
           >
             <i />
           </div>
