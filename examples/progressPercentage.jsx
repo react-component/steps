0447a576d80fd6c9b338d38cffc4694d01ab5884
吴斌 <wb-wb595936@antfin.com>
2020-07-15 19:05:50 +0800

    fix(progressPercentage):修改demo中函数问题

--- examples/progressPercentage.jsx
+++ examples/progressPercentage.jsx
@@ -3,15 +3,12 @@ import '../assets/iconfont.less';
 import React from 'react';
 import Steps, { Step } from '../src';
 
-const description =
-  '这里是多信息的描述啊这里是多信息的描述啊这里是多信息的描述啊这里是多信息的描述啊这里是多信息的描述啊';
-
 export default () => (
-  <Steps progressPercentage={() => <span>a</span>} size="small" current={1}>
-    <Step title="已完成" description={description} onClick={test} />
-    <Step title="进行中" description={description} />
-    <Step title="待运行" description={description} />
-    <Step title="待运行" description={description} />
-    <Step title="待运行" description={description} />
+  <Steps progressPercentage={() => <span>a</span>} current={1}>
+    <Step title="已完成" />
+    <Step title="进行中" />
+    <Step title="待运行" />
+    <Step title="待运行" />
+    <Step title="待运行" />
   </Steps>
 );
