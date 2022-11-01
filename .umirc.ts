07373e4409d80b318d9e9c7336f77ad10c1bab7b
MadCcc <1075746765@qq.com>
2022-11-01 14:10:48 +0800

    chore: add umirc

---
+++ .umirc.ts
@@ -0,0 +1,13 @@
+// more config: https://d.umijs.org/config
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'rc-steps',
+  favicon: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
+  logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
+  outputPath: '.doc',
+  exportStatic: {},
+  base: '/rc-steps',
+  publicPath: '/rc-steps/',
+  hash: true,
+});
