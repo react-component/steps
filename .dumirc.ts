e7340a44f9e1fbb9d4846f671799cc845ecc7748
二货机器人 <smith3816@gmail.com>
2023-12-22 23:11:03 +0800

    chore: build path

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,15 +1,15 @@
-// more config: https://d.umijs.org/config
 import { defineConfig } from 'dumi';
+import path from 'path';
 
 export default defineConfig({
+  alias: {
+    'rc-steps$': path.resolve('src'),
+    'rc-steps/es': path.resolve('src'),
+  },
+  mfsu: false,
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
-    name: 'rc-steps',
+    name: 'Steps',
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
-  outputPath: '.doc',
-  exportStatic: {},
-  base: '/rc-steps',
-  publicPath: '/rc-steps/',
-  hash: true,
 });
